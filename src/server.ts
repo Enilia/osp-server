02c994fd3e0ae9b89b2352b1565447315183bae2
Florian <florian.olivierandre@gmail.com>
2018-12-26 23:51:25 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,32 @@
+import * as Express from 'express'
+import { ServerLoader, ServerSettings } from '@tsed/common'
+import path from 'path'
+import { json } from 'body-parser'
+
+@ServerSettings({
+  rootDir: path.resolve(__dirname),
+  acceptMimes: ['application/json'],
+})
+export class Server extends ServerLoader {
+
+  public $onMountingMiddlewares() {
+
+    const compress = require('compression')
+
+    this
+      .use( json() )
+      .use( compress({}) )
+
+  }
+
+  public $onReady() {
+    console.log( 'Server started...' )
+  }
+
+  public $onServerInitError( err: any ) {
+      console.error( err )
+  }
+
+}
+
+new Server().start()
